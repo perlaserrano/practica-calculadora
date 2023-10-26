35c7b4e27a564f18304eed6b5644aaec4ef04e6d
perla serrano <perlaserrano2015@outlook.com>
2023-10-26 15:58:50 -0400

    Practica-calculadora-js

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+
+function suma() {
+const numero1 = Number(document.getElementById('numero1').value);
+const numero2 = Number(document.getElementById('numero2').value);
+const resultado = numero1 + numero2;
+
+document.getElementById('resultado').value = resultado;
+}
